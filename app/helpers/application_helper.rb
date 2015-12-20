070c95732fe9fc6a646fbd4625d353c649f190b4
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2015-12-21 00:49:07 +0100

    Initial commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,8 @@
+module ApplicationHelper
+  # def stylesheet_link_tag(url, options={})
+  #   puts "before #{url}"
+  #   url = AssetManifest.stylesheet_path(url)
+  #   puts "after #{url}"
+  #   super(url, options)
+  # end
+end
